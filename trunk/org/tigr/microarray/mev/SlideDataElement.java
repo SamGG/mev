ee3cd2c26e136bc76e7536c416451766794387d4
braisted <braisted@96ed72ba-de44-0410-ad2a-a3b678d9004f>
2003-12-08 21:39:28 +0000

    mod

--- trunk/org/tigr/microarray/mev/SlideDataElement.java
+++ trunk/org/tigr/microarray/mev/SlideDataElement.java
@@ -4,8 +4,8 @@ All rights reserved.
 */
 /*
  * $RCSfile: SlideDataElement.java,v $
- * $Revision: 1.1.1.1 $
- * $Date: 2003-08-21 21:04:24 $
+ * $Revision: 1.2 $
+ * $Date: 2003-12-08 21:39:28 $
  * $Author: braisted $
  * $State: Exp $
  */
