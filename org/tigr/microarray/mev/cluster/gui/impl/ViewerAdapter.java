c9e3ec8d50707e608082a15f638fa26f61a2710c
nbhagaba <nbhagaba@96ed72ba-de44-0410-ad2a-a3b678d9004f>
2003-12-09 17:16:43 +0000

    test commit

--- org/tigr/microarray/mev/cluster/gui/impl/ViewerAdapter.java
+++ org/tigr/microarray/mev/cluster/gui/impl/ViewerAdapter.java
@@ -1,44 +1,89 @@
 /*
+
 Copyright @ 1999-2003, The Institute for Genomic Research (TIGR).
+
 All rights reserved.
+
 */
+
 /*
+
  * $RCSfile: ViewerAdapter.java,v $
- * $Revision: 1.2 $
- * $Date: 2003-12-09 16:58:04 $
- * $Author: braisted $
+
+ * $Revision: 1.3 $
+
+ * $Date: 2003-12-09 17:16:43 $
+
+ * $Author: nbhagaba $
+
  * $State: Exp $
+
  */
+
 package org.tigr.microarray.mev.cluster.gui.impl;
+
+
 
 import java.awt.image.BufferedImage;
 
+
+
 import javax.swing.JComponent;
 
+
+
 import org.tigr.microarray.mev.cluster.gui.IViewer;
+
 import org.tigr.microarray.mev.cluster.gui.IFramework;
+
 import org.tigr.microarray.mev.cluster.gui.IData;
+
 import org.tigr.microarray.mev.cluster.gui.IDisplayMenu;
 
+
+
 public class ViewerAdapter implements IViewer {
+
     public BufferedImage getImage() {return null;}
+
     public void onSelected(IFramework framework) {}
+
     public void onDataChanged(IData data) {}
+
     public void onMenuChanged(IDisplayMenu menu) {}
+
     public void onDeselected() {}
+
     public void onClosed() {}
+
     public JComponent getContentComponent() {return null;}
+
     public JComponent getHeaderComponent() {return null;}
+
     
+
     /** Returns a component to be inserted into the scroll pane row header
+
      */
+
     public JComponent getRowHeaderComponent() {return null;}
+
     
+
     /** Returns the corner component corresponding to the indicated corner,
+
      * posibly null
+
      */
+
     public JComponent getCornerComponent(int cornerIndex) {
+
         return null;
+
     }
+
+    //THIS IS A TEST COMMENT
     
+
 }
+
