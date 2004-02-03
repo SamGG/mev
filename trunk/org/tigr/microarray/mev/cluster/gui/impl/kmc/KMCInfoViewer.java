597a742be9b5760b39b1163735c1c219c8ff5a2a
braisted <braisted@96ed72ba-de44-0410-ad2a-a3b678d9004f>
2004-02-03 15:49:10 +0000

    state changes

--- trunk/org/tigr/microarray/mev/cluster/gui/impl/kmc/KMCInfoViewer.java
+++ trunk/org/tigr/microarray/mev/cluster/gui/impl/kmc/KMCInfoViewer.java
@@ -4,8 +4,8 @@ All rights reserved.
 */
 /*
  * $RCSfile: KMCInfoViewer.java,v $
- * $Revision: 1.2 $
- * $Date: 2003-12-08 18:38:38 $
+ * $Revision: 1.3 $
+ * $Date: 2004-02-03 15:49:10 $
  * $Author: braisted $
  * $State: Exp $
  */
@@ -23,7 +23,7 @@ import javax.swing.JTextArea;
 
 import org.tigr.microarray.mev.cluster.gui.impl.ViewerAdapter;
 
-public class KMCInfoViewer extends ViewerAdapter {
+public class KMCInfoViewer extends ViewerAdapter implements java.io.Serializable {
     
     private JComponent header;
     private JTextArea  content;
