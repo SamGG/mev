1398b537fdb855a454db9bdd715fa3fba25b3420
braisted <braisted@96ed72ba-de44-0410-ad2a-a3b678d9004f>
2004-02-03 15:54:32 +0000

    state saving changes

--- org/tigr/microarray/mev/cluster/gui/impl/kmcs/KMCSuppExperimentViewer.java
+++ org/tigr/microarray/mev/cluster/gui/impl/kmcs/KMCSuppExperimentViewer.java
@@ -4,8 +4,8 @@ All rights reserved.
 */
 /*
  * $RCSfile: KMCSuppExperimentViewer.java,v $
- * $Revision: 1.1.1.1 $
- * $Date: 2003-08-21 21:04:25 $
+ * $Revision: 1.2 $
+ * $Date: 2004-02-03 15:54:32 $
  * $Author: braisted $
  * $State: Exp $
  */
@@ -45,6 +45,16 @@ public class KMCSuppExperimentViewer extends ExperimentViewer {
 	getContentComponent().addMouseListener(listener);
 	getHeaderComponent().addMouseListener(listener);
     }
+    
+    private void readObject(java.io.ObjectInputStream ois) throws java.io.IOException, ClassNotFoundException {
+        Listener listener = new Listener();
+        this.popup = createJPopupMenu(listener);
+        getContentComponent().addMouseListener(listener);
+        getHeaderComponent().addMouseListener(listener);        
+    }
+    
+    private void writeObject(java.io.ObjectOutputStream oos) throws java.io.IOException { }
+             
     
     /**
      * Creates a popup menu.
