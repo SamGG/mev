4bd612345933edf39d90a93b4ef3de763281a261
nbhagaba <nbhagaba@96ed72ba-de44-0410-ad2a-a3b678d9004f>
2004-05-03 19:51:35 +0000

    SAMState is now Serializable

--- org/tigr/microarray/mev/cluster/gui/impl/sam/SAMState.java
+++ org/tigr/microarray/mev/cluster/gui/impl/sam/SAMState.java
@@ -16,7 +16,7 @@ import java.util.Vector;
  * @author  nbhagaba
  * @version 
  */
-public class SAMState {
+public class SAMState implements java.io.Serializable {
     
     public static boolean firstRun = true, useAllUniquePerms; 
     public static int[] groupAssignments, numSigGenesByDelta, sortedDArrayIndices;
@@ -32,5 +32,102 @@ public class SAMState {
     /** Creates new SAMState */
     public SAMState() {
     }
-
+    
+    private void writeObject(java.io.ObjectOutputStream oos) throws java.io.IOException {
+        oos.writeBoolean(SAMState.firstRun);
+        oos.writeBoolean(SAMState.useAllUniquePerms);
+        oos.writeObject(SAMState.groupAssignments);
+        oos.writeObject(SAMState.numSigGenesByDelta);
+        oos.writeObject(SAMState.sortedDArrayIndices);
+        oos.writeInt(SAMState.studyDesign);
+        oos.writeInt(SAMState.numCombs);
+        oos.writeInt(SAMState.numNeighbors);
+        oos.writeInt(SAMState.numMultiClassGroups);
+        oos.writeInt(SAMState.numUniquePerms);
+        oos.writeBoolean(SAMState.useKNearest);
+        oos.writeBoolean(SAMState.calculateQLowestFDR);
+        oos.writeBoolean(SAMState.useTusherEtAlS0);
+        oos.writeDouble(SAMState.delta);
+        oos.writeDouble(SAMState.sNought);
+        oos.writeDouble(SAMState.pi0Hat);  
+        oos.writeDouble(SAMState.s0Percentile);
+        oos.writeDouble(SAMState.oneClassMean);
+        oos.writeObject(SAMState.dBarValues);
+        oos.writeObject(SAMState.sortedDArray);
+        oos.writeObject(SAMState.deltaGrid);
+        oos.writeObject(SAMState.medNumFalselyCalledGenesByDelta);
+        oos.writeObject(SAMState.dArray);
+        oos.writeObject(SAMState.rArray);
+        oos.writeObject(SAMState.survivalTimes);
+        oos.writeObject(SAMState.ninetiethPercentileFalselyCalledGenesByDelta);
+        oos.writeObject(SAMState.FDRmedian);
+        oos.writeObject(SAMState.FDR90thPercentile);
+        oos.writeObject(SAMState.qLowestFDR);
+        oos.writeObject(SAMState.inSurvivalAnalysis);
+        oos.writeObject(SAMState.censored);
+        oos.writeObject(SAMState.imputedMatrix);
+        oos.writeObject(SAMState.pairedGroupAExpts);
+        oos.writeObject(SAMState.pairedGroupBExpts);
+    }
+    
+    private void readObject(java.io.ObjectInputStream ois) throws java.io.IOException, ClassNotFoundException {
+        this.firstRun = ois.readBoolean();
+        this.useAllUniquePerms =ois.readBoolean();
+        this.groupAssignments = (int[])ois.readObject();
+        this.numSigGenesByDelta = (int[])ois.readObject();
+        this.sortedDArrayIndices = (int[])ois.readObject();
+        this.studyDesign = ois.readInt();
+        this.numCombs = ois.readInt();
+        this.numNeighbors = ois.readInt();
+        this.numMultiClassGroups = ois.readInt();
+        this.numUniquePerms = ois.readInt();
+        this.useKNearest =ois.readBoolean();
+        this.calculateQLowestFDR = ois.readBoolean();
+        this.useTusherEtAlS0 = ois.readBoolean();
+        this.delta = ois.readDouble();
+        this.sNought = ois.readDouble();
+        this.pi0Hat = ois.readDouble();  
+        this.s0Percentile = ois.readDouble();
+        this.oneClassMean = ois.readDouble();
+        this.dBarValues = (double[])ois.readObject();
+        this.sortedDArray = (double[])ois.readObject();
+        this.deltaGrid = (double[])ois.readObject();
+        this.medNumFalselyCalledGenesByDelta = (double[])ois.readObject();
+        this.dArray = (double[])ois.readObject();
+        this.rArray = (double[])ois.readObject();
+        this.survivalTimes = (double[])ois.readObject();
+        this.ninetiethPercentileFalselyCalledGenesByDelta = (double[])ois.readObject();
+        this.FDRmedian = (double[])ois.readObject();
+        this.FDR90thPercentile = (double[])ois.readObject();
+        this.qLowestFDR = (double[])ois.readObject();
+        this.inSurvivalAnalysis = (boolean[])ois.readObject();
+        this.censored = (boolean[])ois.readObject();
+        this.imputedMatrix = (FloatMatrix)ois.readObject();
+        this.pairedGroupAExpts = (Vector)ois.readObject();
+        this.pairedGroupBExpts = (Vector)ois.readObject();        
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
