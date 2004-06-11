7605a6a723d43f91817624fb4f17a6e480fe7626
braisted <braisted@96ed72ba-de44-0410-ad2a-a3b678d9004f>
2004-06-11 18:41:57 +0000

    New additions

---
+++ trunk/org/tigr/microarray/mev/script/scriptGUI/DiversityRankingInitDialog.java
@@ -0,0 +1,231 @@
+/*
+Copyright @ 1999-2004, The Institute for Genomic Research (TIGR).
+All rights reserved.
+*/
+/*
+ * DiversityRankingInitDialog.java
+ *
+ * Created on May 30, 2004, 11:26 PM
+ */
+
+package org.tigr.microarray.mev.script.scriptGUI;
+
+import java.awt.Frame;
+import java.awt.Color;
+import java.awt.Toolkit;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Component;
+import java.awt.Insets;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+//import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.UIManager;
+import javax.swing.JTextField;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JOptionPane;
+
+import org.tigr.microarray.mev.cluster.gui.impl.dialogs.*;
+import org.tigr.microarray.mev.cluster.gui.impl.dialogs.dialogHelpUtil.*;
+
+import org.tigr.microarray.mev.cluster.gui.impl.dialogs.AlgorithmDialog;
+/**
+ *
+ * @author  braisted
+ */
+public class DiversityRankingInitDialog extends AlgorithmDialog {
+    
+    /** result
+     */    
+    private int result;
+    /** number of clusters input field
+     */    
+    private JTextField numClustersField;
+    /** min cluster size input control
+     */    
+    private JTextField minClusterSizeField;
+    /** selects centroid based diversity method
+     */    
+    private JRadioButton centroidDiversityButton;
+    /** selects gene-to-gene based diversity
+     */    
+    private JRadioButton geneDiversityButton;
+    
+    
+    /** Creates a new instance of DiversityRankingInitDialog
+     * @param parent parent component
+     */
+    public DiversityRankingInitDialog(JFrame parent) {
+        super(parent, "Diversity Ranking Cluster Selection", true);
+        
+        ParameterPanel paramPanel = new ParameterPanel("Selection Parameters");
+        paramPanel.setLayout(new GridBagLayout());
+        
+        JLabel clusterLabel = new JLabel("Desired Number of Clusters");
+        numClustersField = new JTextField("3", 5);
+        JLabel sizeLabel = new JLabel("Minimum Cluster Size (# genes)");
+        minClusterSizeField = new JTextField("10");
+        
+        ButtonGroup bg = new ButtonGroup();
+        centroidDiversityButton = new JRadioButton("Rank Clusters on Centroid Based Diversity", true);
+        centroidDiversityButton.setOpaque(false);
+        bg.add(centroidDiversityButton);
+        centroidDiversityButton.setFocusPainted(false);
+        
+        geneDiversityButton = new JRadioButton("Rank Clusters on Intra-gene Based Diversity");
+        geneDiversityButton.setOpaque(false);
+        bg.add(geneDiversityButton);
+        geneDiversityButton.setFocusPainted(false);
+        
+        paramPanel.add(clusterLabel, new GridBagConstraints(0,0,1,1,0,0,GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0,0,0,10), 0,0));
+        paramPanel.add(numClustersField, new GridBagConstraints(1,0,1,1,0,0,GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0,10,0,0), 0,0));
+        
+        paramPanel.add(sizeLabel, new GridBagConstraints(0,1,1,1,0,0,GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(5,0,0,10), 0,0));
+        paramPanel.add(minClusterSizeField, new GridBagConstraints(1,1,1,1,0,0,GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(5,10,0,0), 0,0));
+        
+        paramPanel.add(centroidDiversityButton, new GridBagConstraints(0,2,2,1,0,0,GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(15,0,0,0), 0,0));
+        paramPanel.add(geneDiversityButton, new GridBagConstraints(0,3,2,1,0,0,GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(5,0,10,0), 0,0));
+        
+        addContent(paramPanel);
+        setActionListeners(new Listener());
+        pack();
+    }
+    
+    /**
+     * Shows the dialog.
+     */
+    public int showModal() {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation((screenSize.width - getSize().width)/2, (screenSize.height - getSize().height)/2);
+        show();
+        return result;
+    }
+    
+    /** returns the desired number of clusters.
+     * @return  */    
+    public int getClusterNumber() {
+        return Integer.parseInt(this.numClustersField.getText());
+    }
+    
+    /** Returns min cluster size desired
+     */    
+    public int getClusterSize() {
+        return Integer.parseInt(this.minClusterSizeField.getText());
+    }
+    
+    /** if true selection is centroid based, else gene-to-gene based.
+     */    
+    public boolean isCentroidBased() {
+        return this.centroidDiversityButton.isSelected();
+    }
+    
+    /** resets controls
+     */    
+    private void resetControls() {
+        this.numClustersField.setText("3");
+        this.minClusterSizeField.setText("10");
+        this.centroidDiversityButton.setSelected(true);
+    }
+    
+    /** validates parameters
+     */    
+    private boolean validateParameters() {
+        int level = 0;
+        int n, c;
+        try {
+            n = Integer.parseInt(this.numClustersField.getText());
+            level++;
+            c = Integer.parseInt(this.minClusterSizeField.getText());
+            level++;
+        } catch (Exception e) {
+            if(level == 0) {
+                JOptionPane.showMessageDialog(this, "The cluster number should be an integer > 0.\n" +
+                "Please enter a new value.", "Parameter Type Mismatch", JOptionPane.ERROR_MESSAGE);
+                this.numClustersField.selectAll();
+                this.numClustersField.grabFocus();
+                return false;
+            } else {
+                JOptionPane.showMessageDialog(this, "The minimum cluster population should be an integer > 0.\n" +
+                "Please enter a new value.", "Parameter Type Mismatch", JOptionPane.ERROR_MESSAGE);
+                this.minClusterSizeField.selectAll();
+                this.minClusterSizeField.grabFocus();
+                return false;
+            }
+        }
+        
+        //type matches, check the values
+        
+        if(n < 1) {
+            JOptionPane.showMessageDialog(this, "The cluster number should be an integer > 0.\n" +
+            "Please enter a new value greater than zero.", "Parameter Value Error", JOptionPane.ERROR_MESSAGE);
+            this.numClustersField.selectAll();
+            this.numClustersField.grabFocus();
+            return false;            
+        } else if(c < 1) {
+            JOptionPane.showMessageDialog(this, "The minimum cluster size should be an integer > 0.\n" +
+            "Please enter a new value greater than zero.", "Parameter Value Error", JOptionPane.ERROR_MESSAGE);
+            this.numClustersField.selectAll();
+            this.numClustersField.grabFocus();
+            return false;
+        }        
+        return true;
+    }
+    
+ /*   public static void main(String [] args) {
+        DiversityRankingInitDialog dialog = new DiversityRankingInitDialog(new JFrame());
+        dialog.showModal();
+    }
+ */   
+    
+    /**
+     * The class to listen to the dialog events.
+     */
+    private class Listener extends DialogListener {
+        
+        public void actionPerformed(ActionEvent e) {
+            String command = e.getActionCommand();
+            
+            if (command.equals("ok-command")) {
+                if(validateParameters()){
+                    dispose();
+                    result = JOptionPane.OK_OPTION;
+                }
+                return;
+            } else if (command.equals("cancel-command")) {
+                result = JOptionPane.CANCEL_OPTION;
+                dispose();
+                return;
+            }
+            else if (command.equals("reset-command")) {
+                resetControls();
+            }
+            else if (command.equals("info-command")){
+                HelpWindow hw = new HelpWindow(DiversityRankingInitDialog.this, "Diversity Ranking Cluster Selection");
+                if(hw.getWindowContent()){
+                    hw.setSize(450,650);
+                    hw.setLocation();
+                    hw.show();
+                }
+                else {
+                    hw.setVisible(false);
+                    hw.dispose();
+                }
+            }
+        }
+        
+        public void windowClosing(WindowEvent e) {
+            result = JOptionPane.CLOSED_OPTION;
+            dispose();
+        }
+    }
+    
+}
---
+++ trunk/org/tigr/microarray/mev/script/scriptGUI/ScriptClusterSelectionInfoViewer.java
@@ -0,0 +1,174 @@
+/*
+Copyright @ 1999-2004, The Institute for Genomic Research (TIGR).
+All rights reserved.
+ */
+/*
+ * $RCSfile: ScriptClusterSelectionInfoViewer.java,v $
+ * $Revision: 1.1 $
+ * $Date: 2004-06-11 18:41:57 $
+ * $Author: braisted $
+ * $State: Exp $
+ */
+package org.tigr.microarray.mev.script.scriptGUI;
+
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextPane;
+
+import org.tigr.microarray.mev.cluster.algorithm.AlgorithmData;
+import org.tigr.microarray.mev.cluster.gui.impl.ViewerAdapter;
+
+/** The ScriptClusterSelectionInfoViewer presents the results of cluster selection algorithms
+ * and the criteria upon which the selections were made.
+ */
+public class ScriptClusterSelectionInfoViewer extends ViewerAdapter implements java.io.Serializable {
+    
+    private JComponent header;
+    private JTextPane  content;
+    private boolean clusterGenes;
+    
+    /** Constructs a <code>ScriptClusterSelectionInfoViewer</code> with specified
+     * clusters and number of genes.
+     * @param algData
+     */
+    public ScriptClusterSelectionInfoViewer(AlgorithmData algData) {
+        header  = createHeader();
+        this.clusterGenes = algData.getParams().getBoolean("process-gene-clusters");
+        content = createContent(algData);
+        setMaxWidth(content, header);
+    }
+    
+    
+    /**
+     * Returns component to be inserted into the framework scroll pane.
+     */
+    public JComponent getContentComponent() {
+        return content;
+    }
+    
+    /**
+     * Returns the viewer header.
+     */
+    public JComponent getHeaderComponent() {
+        return header;
+    }
+    
+    /**
+     * Creates the viewer header.
+     */
+    private JComponent createHeader() {
+        JPanel panel = new JPanel(new GridBagLayout());
+        panel.setBackground(Color.white);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(10, 0, 10, 0);
+        panel.add(new JLabel("<html><body bgcolor='#FFFFFF'><font face='serif' size='5' color='#000080'><b>Cluster Selection Information</b></font></body></html>"), gbc);
+        return panel;
+    }
+    
+    /**
+     * Creates the viewer content component.
+     */
+    private JTextPane createContent(AlgorithmData algData) {
+        
+        String algName = algData.getParams().getString("name");
+        int numOfDesiredClusters = algData.getParams().getInt("desired-cluster-count");
+        String [] div = algData.getStringArray("diversity-value-array");
+        String [] pop = algData.getStringArray("cluster-population-array");
+        int popLimit = algData.getParams().getInt("minimum-cluster-size");
+        
+        JTextPane area = new JTextPane();
+        area.setContentType("text/html");
+        area.setEditable(false);
+        area.setMargin(new Insets(0, 10, 0, 0));
+        
+        String text;
+        
+        int population;
+        
+        if(algName.equals("Diversity Ranking Cluster Selection")) {
+            boolean useCentroid = algData.getParams().getBoolean("use-centroid-based-variability");
+            
+            text = "<html><body bgcolor='#FFFFFF'><font face='serif' size='5'>";
+            text += "<br>Number of Desired Clusters: "+numOfDesiredClusters+"<br>";
+            text += "Minimum Cluster Size (population): "+popLimit+"<br>";
+            if(useCentroid)
+                text += "Diversity Measurement: Centroid Based Diversity (mean gene-to-centroid dist.)<br>";
+            else
+                text += "Diversity Measurement: Intra-gene Based Diversity (mean gene-to-gene dist.)<br>";
+            
+            text += "<br><br>Note: Clusters are sorted by diversity.  Selected clusters are in <b>bold</b> type.<br>";
+            
+            
+            text += "<table cellpadding=10><th><u>Div. Rank</u></th><th><u>Diversity</u></th><th><u>Population</u></td>";
+            int clusterCount = 0;
+            for(int i = 0; i < div.length; i++) {
+                population = Integer.parseInt(pop[i]);
+                if(population >= popLimit && clusterCount < numOfDesiredClusters) {
+                    text += "<tr align=center><td><b>"+(i+1)+"</b></td><td><b>"+div[i]+"</b></td><td><b>"+pop[i]+"</b></td></tr>";
+                    clusterCount++;
+                }
+                else
+                    text += "<tr align=center><td>"+(i+1)+"</td><td>"+div[i]+"</td><td>"+pop[i]+"</td></tr>";
+            }
+                text += "</table></body></html>";
+        } else {
+            boolean useVariance = algData.getParams().getBoolean("use-centroid-variance");
+            
+            text = "<html><body bgcolor='#FFFFFF'><font face='serif' size='5'>";
+            text += "<br>Number of Desired Clusters: "+numOfDesiredClusters+"<br>";
+            text += "Minimum Cluster Size (population): "+popLimit+"<br>";
+            if(useVariance) {
+                text += "Selection Criteria: Centroid Variance<br>";
+                text += "<br><br>Note: Clusters are sorted by decreasing variance.  Selected clusters are in <b>bold</b> type.<br>";
+            text += "<table cellpadding=10><th><u>Var. Rank</u></th><th><u>Variance</u></th><th><u>Population</u></td>";
+            } else {
+                text += "Selection Criteria: Centroid Entropy<br>";            
+                text += "<br><br>Note: Clusters are sorted by decreasing entropy.  Selected clusters are in <b>bold</b> type.<br>";
+            text += "<table cellpadding=10><th><u>Entropy Rank</u></th><th><u>Entropy</u></th><th><u>Population</u></td>";
+            }
+            
+            int clusterCount = 0;
+            for(int i = 0; i < div.length; i++) {
+                population = Integer.parseInt(pop[i]);
+                if(population >= popLimit && clusterCount < numOfDesiredClusters) {
+                    text += "<tr align=center><td><b>"+(i+1)+"</b></td><td><b>"+div[i]+"</b></td><td><b>"+pop[i]+"</b></td></tr>";
+                    clusterCount++;
+                }
+                else
+                    text += "<tr align=center><td>"+(i+1)+"</td><td>"+div[i]+"</td><td>"+pop[i]+"</td></tr>";
+            }
+            text += "</table></body></html>";
+        }
+        
+        area.setText(text);
+        area.setCaretPosition(0);
+        return area;
+    }
+    
+    /**
+     * Synchronize content and header sizes.
+     */
+    private void setMaxWidth(JComponent content, JComponent header) {
+        int c_width = content.getPreferredSize().width;
+        int h_width = header.getPreferredSize().width;
+        if (c_width > h_width) {
+            header.setPreferredSize(new Dimension(c_width, header.getPreferredSize().height));
+        } else {
+            content.setPreferredSize(new Dimension(h_width, content.getPreferredSize().height));
+        }
+    }
+    
+    /** Returns a component to be inserted into the scroll pane row header
+     */
+    public JComponent getRowHeaderComponent() {
+        return null;
+    }
+    
+}
