cb80138fe73d6cb9d5c7bc824977af069a7b9556
braistedj <braistedj@96ed72ba-de44-0410-ad2a-a3b678d9004f>
2005-03-10 20:16:48 +0000

    3.1 release sync.

--- org/tigr/microarray/mev/cluster/NodeValue.java
+++ org/tigr/microarray/mev/cluster/NodeValue.java
@@ -4,8 +4,8 @@ All rights reserved.
 */
 /*
  * $RCSfile: NodeValue.java,v $
- * $Revision: 1.2 $
- * $Date: 2005-02-24 20:24:07 $
+ * $Revision: 1.3 $
+ * $Date: 2005-03-10 20:16:47 $
  * $Author: braistedj $
  * $State: Exp $
  */
