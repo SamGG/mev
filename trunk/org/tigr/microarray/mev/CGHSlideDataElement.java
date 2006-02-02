81fc229b82de244991375545de598d10dc8193f9
raktim <raktim@96ed72ba-de44-0410-ad2a-a3b678d9004f>
2006-02-02 20:01:39 +0000

    CGH etxtension of SlideDataElement

---
+++ trunk/org/tigr/microarray/mev/CGHSlideDataElement.java
@@ -0,0 +1,39 @@
+package org.tigr.microarray.mev;
+
+import org.tigr.microarray.mev.cgh.CGHDataObj.CGHClone;
+
+public class CGHSlideDataElement extends SlideDataElement {
+	public static final int CLONE_ID = 0;
+	public static final int CHR = 1;
+	public static final int START = 2;
+	public static final int END = 3;
+		
+	public  CGHSlideDataElement(String UID, int[] rows, int[] columns, float[] intensities, String[] values){
+		super(UID, rows, columns, intensities, values);
+	}
+	
+	public String getCloneID() {
+		return extraFields[CLONE_ID];
+	}
+	
+	public String getChromosome() {
+		return extraFields[CHR];
+	}
+	
+	public int getProbeStart() {
+		return Integer.parseInt(extraFields[START]);
+	}
+	
+	public int getProbeEnd() {
+		return Integer.parseInt(extraFields[END]);
+	}
+	
+	public int getProbeLength(){
+		return getProbeEnd() - getProbeStart();
+	}
+	
+	public CGHClone getClone(int species) {
+		//System.out.println("getClone(): " + getCloneID() + ", " + getChromosome() + ", " + getProbeStart() + ", " + getProbeEnd());
+		return new CGHClone(getCloneID().trim(), getChromosome(), getProbeStart(), getProbeEnd(), species);
+	}
+}
