64b7c25085f7a5c0d4e2d323890d6cdddeb51fa2
caliente <caliente@96ed72ba-de44-0410-ad2a-a3b678d9004f>
2006-02-08 06:34:58 +0000

    Updated Rama to pop up an indeterminate JProgressBar while R is calculating.

--- trunk/org/tigr/microarray/mev/r/RamaProgress.java
+++ trunk/org/tigr/microarray/mev/r/RamaProgress.java
@@ -6,6 +6,7 @@ package org.tigr.microarray.mev.r;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
@@ -16,7 +17,7 @@ import javax.swing.JProgressBar;
 /**
  * @author iVu
  */
-public class RamaProgress extends Thread {
+public class RamaProgress {
 	private JDialog dialog;
 	private JFrame jf;
 	private JProgressBar bar;
@@ -25,29 +26,48 @@ public class RamaProgress extends Thread {
 	public RamaProgress( JFrame jfP ) {
 		this.jf = jfP;
 		this.dialog = new JDialog( this.jf );
-		this.dialog.setSize( 200, 100 );
+		this.dialog.setSize( 500, 100 );
+		
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout( new BoxLayout( mainPanel, BoxLayout.Y_AXIS ) );
+		
+		//label
+		JLabel progressLabel = new JLabel( "This could take a long time" );
+		JPanel labelPanel = new JPanel();
+		labelPanel.setLayout( new BoxLayout( labelPanel, BoxLayout.X_AXIS ) );
+		labelPanel.add( Box.createHorizontalGlue() );
+		labelPanel.add( progressLabel );
+		labelPanel.add( Box.createHorizontalGlue() );
+		JLabel progressLabel2 = new JLabel( "As a reference, 4 arrays (640 genes) takes about half an hour" );
+		JPanel labelPanel2 = new JPanel();
+		labelPanel2.setLayout( new BoxLayout( labelPanel2, BoxLayout.X_AXIS ) );
+		labelPanel2.add( Box.createHorizontalGlue() );
+		labelPanel2.add( progressLabel2 );
+		labelPanel2.add( Box.createHorizontalGlue() );
+		
+		//progressbar
 		this.bar = new JProgressBar();
 		this.bar.setPreferredSize( new Dimension( 150, 35 ) );
-		bar.setIndeterminate( true );
-		JLabel progressLabel = new JLabel( "This will take a long time" );
+		this.bar.setIndeterminate( true );
+		JPanel barPanel = new JPanel();
+		barPanel.setLayout( new BoxLayout( barPanel, BoxLayout.X_AXIS ) );
+		barPanel.add( Box.createHorizontalGlue() );
+		barPanel.add( bar );
+		barPanel.add( Box.createHorizontalGlue() );
+		barPanel.repaint();
 		
-		JPanel panel = new JPanel();
-		panel.setLayout( new BoxLayout( panel, BoxLayout.Y_AXIS ) );
-		panel.add( new JLabel( "  " ) );
-		panel.add( progressLabel );
-		panel.add( bar );
-		panel.repaint();
+		//add to mainPanel
+		mainPanel.add( Box.createVerticalGlue() );
+		mainPanel.add( labelPanel );
+		mainPanel.add( labelPanel2 );
+		mainPanel.add( Box.createVerticalGlue() );
+		mainPanel.add( barPanel );
 
-		this.dialog.getContentPane().add( panel );
+		this.dialog.getContentPane().add( mainPanel );
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		this.dialog.setLocation((screenSize.width - dialog.getSize().width)/2, 
 				(screenSize.height - dialog.getSize().height)/2);
 		this.dialog.setVisible( true );
-	}
-	
-	
-	public void run() {
-		this.dialog.repaint();
 	}
 	
 	
@@ -56,8 +76,12 @@ public class RamaProgress extends Thread {
 	}
 	
 	
+	public JProgressBar getProgressBar() {
+		return this.bar;
+	}
+	
+	
 	public static void main( String[] args ) {
 		RamaProgress rp = new RamaProgress( new JFrame() );
-		rp.run();
 	}
 }//end class
