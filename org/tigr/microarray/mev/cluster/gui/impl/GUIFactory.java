f95a2769947337bf911ec1e18a70fd026fd8b101
wwang67 <wwang67@96ed72ba-de44-0410-ad2a-a3b678d9004f>
2006-02-24 16:04:27 +0000

    change for cumstomized toolbar

--- org/tigr/microarray/mev/cluster/gui/impl/GUIFactory.java
+++ org/tigr/microarray/mev/cluster/gui/impl/GUIFactory.java
@@ -4,9 +4,9 @@ All rights reserved.
 */
 /*
  * $RCSfile: GUIFactory.java,v $
- * $Revision: 1.4 $
- * $Date: 2006-02-23 20:59:49 $
- * $Author: caliente $
+ * $Revision: 1.5 $
+ * $Date: 2006-02-24 16:04:27 $
+ * $Author: wwang67 $
  * $State: Exp $
  */
 package org.tigr.microarray.mev.cluster.gui.impl;
@@ -19,7 +19,7 @@ import java.util.StringTokenizer;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
-
+import org.tigr.microarray.mev.TMEV;
 import org.tigr.microarray.mev.cluster.gui.AnalysisDescription;
 import org.tigr.microarray.mev.cluster.gui.IGUIFactory;
 
@@ -47,6 +47,7 @@ public class GUIFactory implements IGUIFactory {
 	String names = null;
 	try {
 	    names = bundle.getString("gui.names");
+	   
 	} catch (MissingResourceException e) {
 	    e.printStackTrace();
 	}
@@ -56,7 +57,7 @@ public class GUIFactory implements IGUIFactory {
 	ArrayList list = new ArrayList();
 	StringTokenizer tokenizer = new StringTokenizer(names, ":");
 	String key;
-	String name, clazz, tooltip;
+	String name, clazz,category, tooltip;
 	Icon smallIcon, largeIcon;
 	while (tokenizer.hasMoreTokens()) {
 	    try {
@@ -66,18 +67,57 @@ public class GUIFactory implements IGUIFactory {
 		}
 		name  = bundle.getString(key+".name").trim();
 		clazz = bundle.getString(key+".class").trim();
+		category=bundle.getString(key+".category").trim();
 		tooltip = bundle.getString(key+".tooltip").trim();
 		smallIcon = getIcon(bundle.getString(key+".smallIcon").trim());
 		largeIcon = getIcon(bundle.getString(key+".largeIcon").trim());
-		list.add(new AnalysisDescription(name, clazz, smallIcon, largeIcon, tooltip));
-	    } catch (Exception e) {
+		list.add(new AnalysisDescription(name, clazz,category, smallIcon, largeIcon, tooltip));
+		} catch (Exception e) {
 		System.out.println("Error while reading "+BUNDLE_NAME+".properties file: ");
 		e.printStackTrace();
 	    }
 	}
 	return(AnalysisDescription[])list.toArray(new AnalysisDescription[list.size()]);
     }
+    /*
+    private AnalysisDescription[] createAnalysisDescription() {
+    	String names = null;
+    	try {
+    	    names = bundle.getString("gui.names");
+    	    //System.out.print(names);
+    	} catch (MissingResourceException e) {
+    	    e.printStackTrace();
+    	}
+    	if (names == null) {
+    	    return null;
+    	}
+    	ArrayList list = new ArrayList();
+    	StringTokenizer tokenizer = new StringTokenizer(names, ":");
+    	String key;
+    	String name, clazz, tooltip;
+    	Icon smallIcon, largeIcon;
     
+    	int i=0;
+    	while (tokenizer.hasMoreTokens()) {
+    	    try {
+    		key   = tokenizer.nextToken();
+    		if (key.trim().equals("")) {
+    		    continue;
+    		}
+    		name  = bundle.getString(key+".name").trim();
+    		clazz = bundle.getString(key+".class").trim();
+    		tooltip = bundle.getString(key+".tooltip").trim();
+    		smallIcon = getIcon(bundle.getString(key+".smallIcon").trim());
+    		largeIcon = getIcon(bundle.getString(key+".largeIcon").trim());
+    		list.add(new AnalysisDescription(name, clazz, smallIcon, largeIcon, tooltip));
+    		} catch (Exception e) {
+    		System.out.println("Error while reading "+BUNDLE_NAME+".properties file: ");
+    		e.printStackTrace();
+    	    }
+    	}
+    	return(AnalysisDescription[])list.toArray(new AnalysisDescription[list.size()]);
+        }
+    */
     public static ImageIcon getIcon(String name) {
 	URL url = GUIFactory.class.getResource("/org/tigr/microarray/mev/cluster/gui/impl/images/"+name);
 	if (url == null)
