f00539b42afe9d3d3faf81eb65fa862cc23d7e60
caliente <caliente@96ed72ba-de44-0410-ad2a-a3b678d9004f>
2006-03-03 00:23:34 +0000

    Added bridge

---
+++ trunk/org/tigr/microarray/mev/cluster/algorithm/impl/Bridge.java
@@ -0,0 +1,24 @@
+/*
+ * Created on Oct 6, 2005
+ */
+package org.tigr.microarray.mev.cluster.algorithm.impl;
+
+import org.tigr.microarray.mev.cluster.algorithm.AbstractAlgorithm;
+import org.tigr.microarray.mev.cluster.algorithm.AlgorithmData;
+import org.tigr.microarray.mev.cluster.algorithm.AlgorithmException;
+
+/**
+ * @author iVu
+ */
+public class Bridge extends AbstractAlgorithm {
+
+	
+	public AlgorithmData execute(AlgorithmData data) throws AlgorithmException {
+		return null;
+	}
+
+
+	
+	public void abort() {}
+
+}
