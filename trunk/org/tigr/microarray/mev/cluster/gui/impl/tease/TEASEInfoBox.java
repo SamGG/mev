186db8344e957fc7d559d81b33fcaca2d7e49820
caliente <caliente@96ed72ba-de44-0410-ad2a-a3b678d9004f>
2006-03-07 19:02:07 +0000

    Fixed no hit bug

--- trunk/org/tigr/microarray/mev/cluster/gui/impl/tease/TEASEInfoBox.java
+++ trunk/org/tigr/microarray/mev/cluster/gui/impl/tease/TEASEInfoBox.java
@@ -9,7 +9,6 @@ package org.tigr.microarray.mev.cluster.gui.impl.tease;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Polygon;
 
 import javax.swing.JLabel;
 
@@ -42,7 +41,6 @@ public class TEASEInfoBox extends JLabel{
 	private Color color;     //color of the dots, indicates the significance of the result 
 	private String[][] matrix;   //a reference to the result matrix
 	
-	private Polygon dotgon;
 	
 	/**
 	 * create an instance of TEASEInfoBox
@@ -63,12 +61,16 @@ public class TEASEInfoBox extends JLabel{
 		this.matrix = (String[][])data.getObjectMatrix("result-matrix");
 		this.upperBound = param.getDouble("upper-boundary");
 		this.lowerBound = param.getDouble("lower-boundary");
-		
-		if (this.matrix[0].length == 9) 
-			this.isCluster = true;
-		else 
-			this.isCluster = false;
 
+		//System.out.println("matrix is null: " + (matrix == null));
+//		System.out.println(matrix.length);
+//		System.out.println("matrix[1] is null: " + (matrix[1] == null));
+		if (matrix != null) {
+			if (Double.valueOf(matrix[1][matrix[1].length - 1]).doubleValue() <= 1.1) 
+				this.isCluster = true;
+			else 
+				this.isCluster = false;
+		}
 		setColor();
 	}
 
@@ -77,17 +79,26 @@ public class TEASEInfoBox extends JLabel{
 	 * @return text to be displayed in genesTree panel
 	 */
 	public String getInfoText() {
+		if (matrix == null)
+			return "";
+		
 		String text = "<html><body bgcolor = #ffffcc><table cellspacing = 10>";
+		int midpoint;
+		if (isCluster)
+			midpoint = matrix[0].length - 6;
+		else 
+			midpoint = matrix[0].length - 3;
+		
 		for (int i = 0; i < TEASEInfoBox.displayLimit; i++) {
 			text += "<tr>";
-			for (int j = 1; j < 3; j++) {
+			for (int j = 1; j < midpoint; j++) {
 				text += "<td align = left>" + this.matrix[i][j] + "</td>";
 			}
-			text += "<td align = left><em>" + this.matrix[i][3] + "</em></td>";
-			if (matrix[i].length == 9)
+			text += "<td align = left><em>" + this.matrix[i][midpoint] + "</em></td>";
+			if (isCluster)
 				text += "<td align = right>" + matrix[i][matrix[i].length-1] + "</td>";
 			else 
-				text += "<td align = right>" + matrix[i][4] +  matrix[i][5] + "</td>";
+				text += "<td align = right>" + matrix[i][midpoint+1] +  "\t" + matrix[i][midpoint+2] + "</td>";
 			text += "</tr>";
 		}
 		return text;
@@ -98,24 +109,26 @@ public class TEASEInfoBox extends JLabel{
 	 * @param score
 	 */
 	public void setColor() {
-		if (this.isCluster) {
-			double upperLog = Math.log(this.upperBound);
-			double lowerLog = Math.log(this.lowerBound);
-			double score = Math.log(Double.valueOf(this.matrix[1][this.matrix[1].length - 1]).doubleValue());
-			double middle = (upperLog+lowerLog)/2;
-			int distance;
-
-			if (score < lowerLog) 
-				this.color = new Color(255, 20, 0);
-			else if (score > upperLog)
-				this.color = new Color(0, 20, 255);
-			else if (score < upperLog && score > middle) {
-				distance = (int)(((upperLog - score)/(upperLog-middle)) * 255);
-				this.color = new Color(distance, 20, 255);
-			}
-			else {      //between the two boundaries
-				distance = (int)(((middle - score)/(middle-lowerLog)) * 255);
-				this.color = new Color(255, 20, 255 - distance);
+		if (matrix == null)
+			this.color = Color.BLACK;
+		else if (this.isCluster) {
+				double upperLog = Math.log(this.upperBound);
+				double lowerLog = Math.log(this.lowerBound);
+				double score = Math.log(Double.valueOf(this.matrix[1][this.matrix[1].length - 1]).doubleValue());
+				double middle = (upperLog+lowerLog)/2;
+				int distance;
+	
+				if (score < lowerLog) 
+					this.color = new Color(255, 20, 0);
+				else if (score > upperLog)
+					this.color = new Color(0, 20, 255);
+				else if (score < upperLog && score > middle) {
+					distance = (int)(((upperLog - score)/(upperLog-middle)) * 255);
+					this.color = new Color(distance, 20, 255);
+				}
+				else {      //between the two boundaries
+					distance = (int)(((middle - score)/(middle-lowerLog)) * 255);
+					this.color = new Color(255, 20, 255 - distance);
 			}
 		} else
 			this.color = new Color(234, 72, 173);
