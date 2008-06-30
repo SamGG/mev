506d3e5b2961461dee09100230b6e410ea0e388c
saritanair <saritanair@96ed72ba-de44-0410-ad2a-a3b678d9004f>
2008-06-30 16:34:31 +0000

    Changed the default file location of "Browse" button in annotation panel to TMEV.getDataPath/Annotation

--- source/org/tigr/microarray/mev/file/StanfordFileLoader.java
+++ source/org/tigr/microarray/mev/file/StanfordFileLoader.java
@@ -763,7 +763,7 @@ public class StanfordFileLoader extends ExpressionFileLoader {
         		FileLoaderUtility fileLoad = new FileLoaderUtility();
         		File selectedFile;
         		JFileChooser fileChooser = new JFileChooser(
-        				SuperExpressionFileLoader.DATA_PATH);
+        				SuperExpressionFileLoader.ANNOTATION_PATH);
         		fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
         		int retVal = fileChooser.showOpenDialog(StanfordFileLoaderPanel.this);
 
