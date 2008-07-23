e3ce8176d62482f9a57189b9bfecd88e06d03f2b
saritanair <saritanair@96ed72ba-de44-0410-ad2a-a3b678d9004f>
2008-07-23 20:20:43 +0000

    New class for Chip Annotation constants

---
+++ trunk/source/org/tigr/microarray/mev/annotation/ChipAnnotationFieldConstants.java
@@ -0,0 +1,13 @@
+package org.tigr.microarray.mev.annotation;
+
+public class ChipAnnotationFieldConstants {
+	public static final String NOT_AVAILABLE = "NA";
+
+	public static final String CHIP_TYPE = "chip-type";
+	public static final String CHIP_NAME = "chip-name";
+	public static final String SPECIES_NAME = "species-name";
+	public static final String GENOME_BUILD = "genome-build";
+	public static final String ANNOTATION_FILE_NAME = "annotation-file-name";
+	public static final String DATA_TYPE = "data-type";
+
+}
