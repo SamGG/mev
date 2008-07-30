f3881378002860c5e23cb4888c39e022ea49022c
eleanorahowe <eleanorahowe@96ed72ba-de44-0410-ad2a-a3b678d9004f>
2008-07-30 13:35:52 +0000

    Updated copyright notices.

--- TMEV.bat
+++ TMEV.bat
@@ -1,10 +1,12 @@
 @echo off
 goto CMD
 
-Modified for MeV v. 4.1, 12.12.2007
+Modified for MeV v. 4.2, 07.31.2000
 
 ***********************************************************************
-Copyright @ 1999-2005, The Institute for Genomic Research (TIGR).
+Copyright (c) 1999-2005 The Institute for Genomic Research (TIGR).
+Copyright (c) 2005-2008, the Dana-Farber Cancer Institute (DFCI), 
+J. Craig Venter Institute (JCVI) and the University of Washington.
 All rights reserved.
 ***********************************************************************
  $RCSfile: TMEV.bat,v $
