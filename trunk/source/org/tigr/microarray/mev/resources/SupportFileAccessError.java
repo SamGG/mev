690e3b173b8fac15bdfb1cf0f446ec3868e9d4c6
eleanorahowe <eleanorahowe@96ed72ba-de44-0410-ad2a-a3b678d9004f>
2008-11-03 19:06:14 +0000

    Added copyright notices to files.

--- trunk/source/org/tigr/microarray/mev/resources/SupportFileAccessError.java
+++ trunk/source/org/tigr/microarray/mev/resources/SupportFileAccessError.java
@@ -1,3 +1,9 @@
+/*******************************************************************************
+ * Copyright (c) 1999-2005, The Institute for Genomic Research (TIGR).
+ * Copyright (c) 1999, 2008, the Dana-Farber Cancer Institute (DFCI), 
+ * the J. Craig Science Foundataion (JCVI), and the University of Washington.
+ * All rights reserved.
+ *******************************************************************************/
 package org.tigr.microarray.mev.resources;
 
 public class SupportFileAccessError extends Exception {
