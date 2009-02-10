127004abc5da0ba20e9a58b7cf8fdd0e0459b99e
dschlauch <dschlauch@96ed72ba-de44-0410-ad2a-a3b678d9004f>
2009-02-10 22:58:23 +0000

    Added many calculation improvements, expression probility differences and a a little GUI for testing

--- trunk/source/org/tigr/microarray/mev/cluster/gui/impl/bn/BNProbTableCalculator.java
+++ trunk/source/org/tigr/microarray/mev/cluster/gui/impl/bn/BNProbTableCalculator.java
@@ -1,8 +1,18 @@
 package org.tigr.microarray.mev.cluster.gui.impl.bn;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import javax.swing.*;
+
 public class BNProbTableCalculator {
+	private float[][] origProbTable;
+	private float[][] difProbTable;
 	private float[][] probTable;
+	private int[] setNodes;
 	private boolean[] ptFound;
 	private BifDOMBuilder bdb;
 	ArrayList<BifNode> bif= new ArrayList<BifNode>();
@@ -15,14 +25,18 @@ public class BNProbTableCalculator {
 			e.printStackTrace();
 		}
 		numNodes=bif.size();
+		origProbTable = new float[3][numNodes];
+		difProbTable = new float[3][numNodes];
 		probTable = new float[3][numNodes];
 		ptFound = new boolean[numNodes];
-		for (int i=0; i<numNodes; i++){
-			ptFound[i]= false;
-		}
-//		printbdb();
+		setNodes = new int[numNodes];
 		calculateTables();
-		printPTs();
+		for (int i=0; i<3; i++){
+			for (int j=0; j<numNodes; j++){
+				origProbTable[i][j] = probTable[i][j];
+			}
+		}
+		getDifTable();
 	}
 	
 	/**
@@ -30,8 +44,30 @@ public class BNProbTableCalculator {
 	 */
 	private void calculateTables(){
 		for (int i=0; i<numNodes; i++){
+			ptFound[i]= false;
+		}
+		for (int i=0; i<numNodes; i++){
 			if(!havePT(bif.get(i)))
 				getPT(bif.get(i));
+		}
+		getDifTable();
+	}
+	
+	private void resetNodes(){
+		for (int i=0; i<setNodes.length; i++){
+			setNodes[i]=0;
+		}
+	}
+
+	private void setNode(BifNode bifNode, int set){
+		setNodes[bif.indexOf(bifNode)]=set;
+	}
+	
+	private void getDifTable(){
+		for (int i=0; i<3; i++){
+			for (int j=0; j<numNodes; j++){
+				difProbTable[i][j] = probTable[i][j]-origProbTable[i][j];
+			}
 		}
 	}
 	
@@ -57,6 +93,14 @@ public class BNProbTableCalculator {
 	 */
 	private void calcPTWithParents(BifNode bifNode){
 		ArrayList<BifNode> parentNodes = bdb.getParents(bifNode);
+		if(setNodes[bif.indexOf(bifNode)]!=0){
+			for (int bin=0; bin<3; bin++){
+				probTable[bin][bif.indexOf(bifNode)]=0;
+			}
+			probTable[setNodes[bif.indexOf(bifNode)]-1][bif.indexOf(bifNode)]=1;
+			ptFound[bif.indexOf(bifNode)]=true;
+			return;
+		}
 		if (parentNodes.size()==0){
 			for (int bin=0; bin<3; bin++){
 				try{
@@ -110,9 +154,9 @@ public class BNProbTableCalculator {
 	}
 	
 	/**
-	 * Prints a table listing Probabilities of each state for each node.
+	 * Prints a table listing the current probabilities of each state for each node.
 	 */
-	private void printPTs(){
+	private void printCurrentPTs(){
 		System.out.println();
 		System.out.println("  ********Results*******");
 		for (int i=0; i<this.numNodes; i++){
@@ -123,7 +167,34 @@ public class BNProbTableCalculator {
 			System.out.println();
 		}
 	}
-	
+	/**
+	 * Prints a table listing the original probabilities of each state for each node.
+	 */
+	private void printOriginalPTs(){
+		System.out.println();
+		System.out.println("  ********Original*******");
+		for (int i=0; i<this.numNodes; i++){
+			System.out.print("Node: "+ bif.get(i).getChild()+":     \t");
+			for (int j=0; j<3; j++){
+				System.out.print(origProbTable[j][i]+"\t");
+			}
+			System.out.println();
+		}
+	}
+	/**
+	 * Prints the difference table listing the probability changes of each state for each node from the original.
+	 */
+	private void printDifPTs(){
+		System.out.println();
+		System.out.println("  ********Difference*******");
+		for (int i=0; i<this.numNodes; i++){
+			System.out.print("Node: "+ bif.get(i).getChild()+":     \t");
+			for (int j=0; j<3; j++){
+				System.out.print(difProbTable[j][i]+"\t");
+			}
+			System.out.println();
+		}
+	}
 	/**
 	 * Prints info for each node
 	 */
@@ -154,48 +225,85 @@ public class BNProbTableCalculator {
 			}
 		}
 	}
+	private void showDialog(){
+
+		JPanel jp = new JPanel();
+        jp.setLayout(new GridBagLayout());
+		for (int i=0; i<this.numNodes; i++){
+			jp.add(new JLabel("Node: "+ this.bif.get(i).getChild()+": "), new GridBagConstraints(0,i,1,1,0,0,GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(5,0,5,0), 0,0));
+	        JButton jb = new JButton("Down");
+	        jb.setActionCommand(String.valueOf(i));
+	        jb.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent evt) {
+            		setNode(bif.get(Integer.parseInt(evt.getActionCommand())), 1);
+            		calculateTables();
+            		printCurrentPTs();
+            		printDifPTs();
+                }
+	        });
+			jp.add(jb, new GridBagConstraints(1,i,1,1,0,0,GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(5,0,5,0), 0,0));
+			jb = new JButton("Neutral");
+	        jb.setActionCommand(String.valueOf(i));
+	        jb.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent evt) {
+                	setNode(bif.get(Integer.parseInt(evt.getActionCommand())), 2);
+            		calculateTables();
+            		printCurrentPTs();
+            		printDifPTs();
+                }
+	        });
+			jp.add(jb, new GridBagConstraints(2,i,1,1,0,0,GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(5,0,5,0), 0,0));
+			jb = new JButton("Up");
+	        jb.setActionCommand(String.valueOf(i));
+	        jb.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent evt) {
+                	setNode(bif.get(Integer.parseInt(evt.getActionCommand())), 3);
+            		calculateTables();
+            		printCurrentPTs();
+            		printDifPTs();
+                }
+	        });
+			jp.add(jb, new GridBagConstraints(3,i,1,1,0,0,GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(5,0,5,0), 0,0));
+		}
+		
+		JButton jb = new JButton("Reset");
+        jb.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent evt) {
+				resetNodes();
+        		calculateTables();
+        		printCurrentPTs();
+        		printDifPTs();
+            }
+        });
+		jp.add(jb, new GridBagConstraints(1,numNodes,1,1,0,0,GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(5,0,5,0), 0,0));
+	
+		JDialog jd = new JDialog();
+		jd.add(jp);
+		jd.pack();
+		jd.setSize(400, 800);
+		jd.setVisible(true);
+	}
 	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		BifDOMBuilder bdb = new BifDOMBuilder();
 		BNProbTableCalculator tableCalc = new BNProbTableCalculator(bdb);
-//		
-//		try {
-//			int count = 0;
-//			ArrayList<BifNode> bif= new ArrayList<BifNode>();
-//			bif = bdb.build("C:/workspace/data/BN_files/resultBif1.xml");
-////			float[][][] aa =bif.get(0).getCPT3D();
-//			for (int i=0; i<bif.size(); i++ ){
-//				System.out.println("\n"+i);
-//				float[] a =bif.get(i).getCPT();
-//				String b =bif.get(i).getChild();
-//				ArrayList<String> c = 	bif.get(i).getParents();
-//				if (b!=null){
-//					System.out.println("Child = " +b);
-//				}
-//				if (c!=null){
-//					System.out.print("Parents:\t");
-//					for (int j=0; j<c.size();j++){
-//						System.out.print(c.get(j)+"\t");
-//					}
-//					System.out.println();
-//				}
-//				if (a!=null){
-//					System.out.print("Node "+count+"\t");
-//					count++;
-//					for (int j=0; j<a.length;j++){
-//						System.out.print(a[j]+"\t");
-//					}
-//					System.out.println();
-//				}
-//			}
-//			tableCalc.calculateTables();
-//		} catch (Exception e){
-//			e.printStackTrace();
-//		}
+		tableCalc.printbdb();
+		tableCalc.calculateTables();
+//		tableCalc.setNode(tableCalc.bif.get(0), 2);
+//		tableCalc.calculateTables();
+		
+		tableCalc.showDialog();
+		
+		
+		
+		
+		System.out.println("Set "+tableCalc.bif.get(3).getChild());
+		tableCalc.printCurrentPTs();
+		tableCalc.printOriginalPTs();
+		tableCalc.printDifPTs();
 	}
 
 }
