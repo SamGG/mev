e1f561f45dfab541327cc762fe33607d97b08435
eleanorahowe <eleanorahowe@96ed72ba-de44-0410-ad2a-a3b678d9004f>
2009-08-27 20:33:06 +0000

    Java v1.5 compatibility fix.

--- source/org/tigr/microarray/mev/resources/FTPFileDownloader.java
+++ source/org/tigr/microarray/mev/resources/FTPFileDownloader.java
@@ -79,7 +79,7 @@ public class FTPFileDownloader extends FileDownloader {
 				}
 			}
 		} catch (FtpException ftpe) {
-			throw new IOException(ftpe);
+			throw new IOException(ftpe.getMessage());
 		} catch (NullPointerException npe) {
 			throw new IOException(npe);
 		}finally {
