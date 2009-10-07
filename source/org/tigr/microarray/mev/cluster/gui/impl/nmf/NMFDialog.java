0426b5dbfdc6b12019b8322e53c6bb38431efd58
dschlauch <dschlauch@96ed72ba-de44-0410-ad2a-a3b678d9004f>
2009-10-07 21:23:13 +0000

    new NMF init dialog

--- source/org/tigr/microarray/mev/cluster/gui/impl/nmf/NMFDialog.java
+++ source/org/tigr/microarray/mev/cluster/gui/impl/nmf/NMFDialog.java
@@ -18,6 +18,7 @@ import java.awt.Frame;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Toolkit;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
@@ -31,7 +32,6 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
-import javax.swing.UIManager;
 import javax.swing.border.EtchedBorder;
 
 import org.tigr.microarray.mev.cluster.gui.impl.dialogs.AlgorithmDialog;
@@ -48,9 +48,10 @@ public class NMFDialog extends AlgorithmDialog {
 	private int result = JOptionPane.CANCEL_OPTION;
     
     private SampleSelectionPanel clusteringSelectionPanel;
-    JTextField numRunsField, numClustersField, numItersField;
-    JRadioButton divergenceButton1, clusterBySamples;
-    JCheckBox clustercb;
+    JTextField numRunsField, numClustersField, maxNumClustersField, numItersField;
+    JRadioButton divergenceButton1, clusterBySamples, expScale;
+    JCheckBox clustercb, multiFactors;
+    JLabel numClustersLabel;
     
     /** Creates new NMFDialog */
     public NMFDialog(Frame frame) {
@@ -62,21 +63,18 @@ public class NMFDialog extends AlgorithmDialog {
         GridBagLayout gridbag = new GridBagLayout();
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.fill = GridBagConstraints.HORIZONTAL;       
+        constraints.insets = new Insets(5,0,5,0);
         
         JPanel pane = new JPanel();
         pane.setBackground(Color.white);
         pane.setBorder(new EtchedBorder());
         pane.setLayout(gridbag);
         
-        clusteringSelectionPanel = new SampleSelectionPanel(Color.white, UIManager.getColor("Label.foreground"),true,"Clustering Selection");
-        clusteringSelectionPanel.setGeneButtonItemListener(new EventListener());
-        clusteringSelectionPanel.setSampleButtonItemListener(new EventListener());
-        buildConstraints(constraints, 0, 0, 2, 1, 100, 50);
-        gridbag.setConstraints(clusteringSelectionPanel, constraints);
-        
-
         JPanel clusterTypePanel = new JPanel();
         clusterTypePanel.setBackground(Color.white);
+        clusterTypePanel.setBorder(new javax.swing.border.TitledBorder(new javax.swing.border.EtchedBorder(), "Samples/Genes Selection", 
+                javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, 
+                new java.awt.Font("Dialog", 1, 12), Color.black));  
         GridBagLayout grid = new GridBagLayout();
         
         clusterBySamples= new JRadioButton("Cluster Samples");
@@ -97,82 +95,102 @@ public class NMFDialog extends AlgorithmDialog {
         bg.add(clusterBySamples);
         bg.add(clusterByGenes);
         
-        
-        
-        
+        buildConstraints(constraints, 0, 0, 1, 1, 0, 50);
+        gridbag.setConstraints(clusterTypePanel, constraints);
         pane.add(clusterTypePanel);   
         
-        JPanel numRunsPanel = new JPanel();
-        numRunsPanel.setBackground(Color.white);
-        grid = new GridBagLayout();
-        
-        JLabel numRunsLabel= new JLabel("Number of runs :");
+        JPanel runParams = new JPanel();
+        runParams.setBackground(Color.white);
+        runParams.setBorder(new javax.swing.border.TitledBorder(new javax.swing.border.EtchedBorder(), "Run Parameters", 
+                javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, 
+                new java.awt.Font("Dialog", 1, 12), Color.black));  
+        runParams.setLayout(grid);
+
+        multiFactors = new JCheckBox("Run Multiple Factors");
+        multiFactors.setBackground(Color.white);
+        multiFactors.setSelected(false);
+        multiFactors.addActionListener(new ActionListener(){
+        	public void actionPerformed(ActionEvent ae){
+    			maxNumClustersField.setEnabled(multiFactors.isSelected());
+    			numClustersLabel.setText(multiFactors.isSelected() ? "Cluster range :":"Number of clusters :");
+        	}
+        });
         buildConstraints(constraints, 0, 0, 1, 1, 50, 100);
+        grid.setConstraints(multiFactors, constraints);
+        runParams.add(multiFactors);  
+        
+        JLabel numRunsLabel= new JLabel("Number of runs : ");
+        buildConstraints(constraints, 0, 1, 1, 1, 50, 100);
+        constraints.fill = GridBagConstraints.NONE;       
+        constraints.anchor = GridBagConstraints.EAST;
         grid.setConstraints(numRunsLabel, constraints);
-        numRunsPanel.add(numRunsLabel);     
+        runParams.add(numRunsLabel);     
+        constraints.fill = GridBagConstraints.HORIZONTAL;       
         
         numRunsField = new JTextField("10", 7);
-        buildConstraints(constraints, 1, 0, 1, 1, 50, 100);
+        buildConstraints(constraints, 1, 1, 1, 1, 50, 100);
         grid.setConstraints(numRunsField, constraints);
-        numRunsPanel.add(numRunsField);    
+        runParams.add(numRunsField);    
         
-        buildConstraints(constraints, 0, 1, 1, 1, 0, 50);
-        gridbag.setConstraints(numRunsPanel, constraints);
-        pane.add(numRunsPanel);   
-
-        JPanel numClustersPanel = new JPanel();
-        numClustersPanel.setBackground(Color.white);
-        grid = new GridBagLayout();
-        
-        JLabel numClustersLabel= new JLabel("Number of clusters :");
-        buildConstraints(constraints, 0, 0, 1, 1, 50, 100);
+        numClustersLabel= new JLabel("Number of clusters : ");
+        constraints.fill = GridBagConstraints.NONE;       
+        buildConstraints(constraints, 0, 2, 1, 1, 50, 100);
         grid.setConstraints(numClustersLabel, constraints);
-        numClustersPanel.add(numClustersLabel);     
+        runParams.add(numClustersLabel);     
+        constraints.fill = GridBagConstraints.HORIZONTAL;  
         
-        numClustersField = new JTextField("4", 7);
-        buildConstraints(constraints, 2, 0, 1, 1, 50, 100);
+        numClustersField = new JTextField("2", 7);
+        buildConstraints(constraints, 1, 2, 1, 1, 50, 100);
         grid.setConstraints(numClustersField, constraints);
-        numClustersPanel.add(numClustersField);    
-        
-        buildConstraints(constraints, 0, 2, 1, 1, 0, 50);
-        gridbag.setConstraints(numClustersPanel, constraints);
-        pane.add(numClustersPanel); 
+        runParams.add(numClustersField);    
 
-        JPanel numItersPanel = new JPanel();
-        numItersPanel.setBackground(Color.white);
-        grid = new GridBagLayout();
+        JLabel maxNumClustersLabel= new JLabel(" - ");
+        maxNumClustersLabel.setEnabled(false);
+        buildConstraints(constraints, 2, 2, 1, 1, 50, 100);
+        grid.setConstraints(maxNumClustersLabel, constraints);
+        runParams.add(maxNumClustersLabel);   
+
+        maxNumClustersField = new JTextField("4", 7);
+        maxNumClustersField.setEnabled(false);
+        buildConstraints(constraints, 3, 2, 1, 1, 50, 100);
+        grid.setConstraints(maxNumClustersField, constraints);
+        runParams.add(maxNumClustersField);    
         
-        JLabel numItersLabel= new JLabel("Maximum iterations :");
-        buildConstraints(constraints, 0, 0, 1, 1, 50, 100);
+        JLabel numItersLabel= new JLabel("Maximum iterations : ");
+        constraints.fill = GridBagConstraints.NONE;       
+        buildConstraints(constraints, 0, 3, 1, 1, 50, 100);
         grid.setConstraints(numItersLabel, constraints);
-        numItersPanel.add(numItersLabel);     
+        runParams.add(numItersLabel);     
+        constraints.fill = GridBagConstraints.HORIZONTAL;  
         
         numItersField = new JTextField("1000", 7);
-        buildConstraints(constraints, 1, 0, 1, 1, 50, 100);
+        buildConstraints(constraints, 1, 3, 1, 1, 50, 100);
         grid.setConstraints(numItersField, constraints);
-        numItersPanel.add(numItersField);    
+        runParams.add(numItersField);    
         
-        buildConstraints(constraints, 0, 3, 1, 1, 0, 50);
-        gridbag.setConstraints(numItersPanel, constraints);
-        pane.add(numItersPanel);   
+        buildConstraints(constraints, 0, 2, 1, 1, 0, 50);
+        gridbag.setConstraints(runParams, constraints);
+        pane.add(runParams);  
 
         JPanel divergencePanel = new JPanel();
         divergencePanel.setBackground(Color.white);
-        grid = new GridBagLayout();
-        
-        divergenceButton1= new JRadioButton("Euclidean Cost Measurement");
-        divergenceButton1.setSelected(false);
-        divergenceButton1.setBackground(Color.white);
-        buildConstraints(constraints, 0, 0, 1, 1, 50, 100);
-        grid.setConstraints(divergenceButton1, constraints);
-        divergencePanel.add(divergenceButton1);     
-
-        JRadioButton divergenceButton2= new JRadioButton(" Divergence Cost Measurement");
+        divergencePanel.setBorder(new javax.swing.border.TitledBorder(new javax.swing.border.EtchedBorder(), "Update rules and cost measurement", 
+                javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, 
+                new java.awt.Font("Dialog", 1, 12), Color.black));  
+           
+        JRadioButton divergenceButton2= new JRadioButton(" Divergence");
         divergenceButton2.setBackground(Color.white);
         divergenceButton2.setSelected(true);
-        buildConstraints(constraints, 0, 1, 1, 1, 50, 100);
+        buildConstraints(constraints, 0, 0, 1, 1, 50, 100);
         grid.setConstraints(divergenceButton2, constraints);
         divergencePanel.add(divergenceButton2);  
+
+        divergenceButton1= new JRadioButton("Euclidean");
+        divergenceButton1.setSelected(false);
+        divergenceButton1.setBackground(Color.white);
+        buildConstraints(constraints, 0, 1, 1, 1, 50, 100);
+        grid.setConstraints(divergenceButton1, constraints);
+        divergencePanel.add(divergenceButton1);  
         
         ButtonGroup bg1 = new ButtonGroup();
         bg1.add(divergenceButton1);
@@ -182,10 +200,50 @@ public class NMFDialog extends AlgorithmDialog {
         gridbag.setConstraints(divergencePanel, constraints);
         pane.add(divergencePanel); 
         
+        JPanel normalizationPanel = new JPanel();
+        normalizationPanel.setBackground(Color.white);
+        normalizationPanel.setBorder(new javax.swing.border.TitledBorder(new javax.swing.border.EtchedBorder(), "Data matrix pre-processing", 
+                javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, 
+                new java.awt.Font("Dialog", 1, 12), Color.black));  
+        normalizationPanel.setLayout(grid);
+
+        JLabel noNegs1 = new JLabel("The data matrix may not contain negative values.");
+        JLabel noNegs2 = new JLabel("If negative values exist, select a way to adjust the data.");
+        buildConstraints(constraints, 0, 0, 2, 1, 50, 100);
+        grid.setConstraints(noNegs1, constraints);
+        buildConstraints(constraints, 0, 1, 2, 1, 50, 100);
+        grid.setConstraints(noNegs2, constraints);
+        normalizationPanel.add(noNegs1);     
+        normalizationPanel.add(noNegs2);     
+        
+        constraints.insets = new Insets(10,0,10,0);
+        JRadioButton subtractMin= new JRadioButton("Subtract minimum value");
+        subtractMin.setBackground(Color.white);
+        subtractMin.setSelected(true);
+        buildConstraints(constraints, 0, 2, 1, 1, 50, 100);
+        grid.setConstraints(subtractMin, constraints);
+        normalizationPanel.add(subtractMin);  
+
+        expScale = new JRadioButton("Exponentially scale");
+        expScale.setSelected(false);
+        expScale.setBackground(Color.white);
+        buildConstraints(constraints, 1, 2, 1, 1, 50, 100);
+        grid.setConstraints(expScale, constraints);
+        normalizationPanel.add(expScale);  
+        constraints.insets = new Insets(0,0,0,0);
+        
+        ButtonGroup bg2 = new ButtonGroup();
+        bg2.add(expScale);
+        bg2.add(subtractMin);
+        
+        buildConstraints(constraints, 0, 5, 1, 1, 0, 50);
+        gridbag.setConstraints(normalizationPanel, constraints);
+        pane.add(normalizationPanel); 
+        
         clustercb = new JCheckBox("Store results as clusters");
         clustercb.setBackground(Color.white);
         clustercb.setSelected(false);
-        buildConstraints(constraints, 0, 5, 1, 1, 50, 100);
+        buildConstraints(constraints, 0, 6, 1, 1, 50, 100);
         gridbag.setConstraints(clustercb, constraints);
         pane.add(clustercb);  
         
@@ -244,6 +302,10 @@ public class NMFDialog extends AlgorithmDialog {
 
 	public boolean isDoSamples() {
 		return clusterBySamples.isSelected();
+	}
+	
+	public boolean isExpScale() {
+		return expScale.isSelected();
 	}
     
     protected class EventListener implements ActionListener, ItemListener {
